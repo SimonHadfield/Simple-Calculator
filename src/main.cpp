0ff2c3ad6d8373e42c2df48359aed5ecada173da
Simon G Hadfield <simonguyhadfield@gmail.com>
2023-10-14 10:42:45 +0100

    add header files for imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <iostream>
 #include <cmath>
 #include "MathFunctions.h"
+#include "imgui.h"
+#include "backends/imgui_impl_glfw.h"
+#include "backends/imgui_impl_opengl3.h"
 
 int main()
 {
+	// Initialize GLFW and create a window
+	// ..
+	// 
+	// ImGui::CreateContext();
+
 	std::cout << "addition/subtraction: " << add(100.3453, -30.0234) << std::endl;
 	std::cout << "multiplication: " << multiply(34.5, 2.35) << std::endl;
 	std::cout << "division: " << division(10, 4) << std::endl;
