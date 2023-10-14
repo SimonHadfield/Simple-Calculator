fcb04d1c7db6bba30441398f576ecf8d7a9b9bbd
Simon G Hadfield <simonguyhadfield@gmail.com>
2023-10-14 11:38:05 +0100

    Add glfw and opengl

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <cmath>
 #include "MathFunctions.h"
+
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
 #include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
+//#include "backends/imgui_impl_glfw.h"
+//#include "backends/imgui_impl_opengl3.h"
 
 int main()
 {
 	// Initialize GLFW and create a window
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
 	// ..
 	// 
-	// ImGui::CreateContext();
+	//ImGui::CreateContext();
 
 	std::cout << "addition/subtraction: " << add(100.3453, -30.0234) << std::endl;
 	std::cout << "multiplication: " << multiply(34.5, 2.35) << std::endl;
