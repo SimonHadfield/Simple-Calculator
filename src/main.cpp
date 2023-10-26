4e91851c5d4a7664eb07024fe24c4302e24a8dfa
Simon G Hadfield <simonguyhadfield@gmail.com>
2023-10-26 17:59:19 +0100

    rm MathFunctions and move respective code into main.cpp. Removed unused libraries such as stb

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 #include <cmath>
-#include "MathFunctions.h"
 
 #include "glad/glad.h"
 #include "GLFW/glfw3.h"
@@ -8,6 +7,47 @@
 #include "imgui.h"
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
+
+
+// math functions
+
+long double add(long double x, long double y)
+{
+	return x + y;
+}
+
+long double multiply(long double x, long double y)
+{
+	return x * y;
+}
+
+long double power(long double x, long double exp)
+{
+	return pow(x, exp);
+}
+
+long mod(long double x, long double divisor)
+{
+	return divisor != 0 ? (long)x % (long)divisor : 0.0;
+}
+
+long double division(long double x, long double divisor)
+{
+	return divisor != 0 ? x / divisor : 0.0;
+}
+
+long double factorial(unsigned int x)
+{
+	// base cases
+	if (x == 0) { return 1; } // 0! = 1
+	if (x == 1) { return 1; } // 1! = 1
+	else if (x > 1)
+	{
+		return x * factorial(x - 1);
+	}
+}
+
+//
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
